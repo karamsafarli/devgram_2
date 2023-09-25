4c63bfd6d187143087e710f43ab4e624da270b7b
karamsafarli <karamslee007@gmail.com>
2023-09-25 21:34:36 +0400

    Added environment variables

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,12 +3,14 @@ import '@styles/style.scss';
 import { Providers } from '@redux/provider';
 import SessionProviders from '@components/Provider';
 import App from '@components/App';
+import { getServerSession } from 'next-auth';
 
 import { v2 as cloudinary } from 'cloudinary';
+
 cloudinary.config({
-    cloud_name: 'di9mpt7gd',
-    api_key: '882998772671579',
-    api_secret: 'v6crZDTlZ5QwyhSmjKcB6OcSP8M'
+    cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 export const metadata = {
@@ -16,9 +18,12 @@ export const metadata = {
     description: 'Instagram for developers!',
 }
 
-const RootLayout = ({ children }) => {
+const RootLayout = async ({ children }) => {
+
+    const session = await getServerSession();
+
     return (
-        <SessionProviders>
+        <SessionProviders session={session}>
             <Providers>
                 <App>
                     {children}
