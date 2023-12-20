79988509d837ccf579a2f3edc34dcb87e632c375
karamsafarli <karamslee007@gmail.com>
2023-12-21 00:47:12 +0400

    updated

--- app/api/gemini/route.js
+++ app/api/gemini/route.js
@@ -2,14 +2,18 @@ import { NextResponse } from "next/server";
 import { GoogleGenerativeAI } from '@google/generative-ai'
 
 export const POST = async (req, res) => {
-    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-    const { prompt, imageParts } = await req.json();
-    // console.log(imageParts)
-    const modelName = imageParts.length > 0 ? "gemini-pro-vision" : "gemini-pro"
-    const model = genAI.getGenerativeModel({ model: modelName });
-    const data = imageParts.length > 0 ? [prompt, ...imageParts] : prompt;
-    const result = await model.generateContent(data);
-    const response = await result.response;
-    const text = response.text();
-    return NextResponse.json(text);
+    try {
+        const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+        const { prompt, imageParts } = await req.json();
+        // console.log(imageParts)
+        const modelName = imageParts.length > 0 ? "gemini-pro-vision" : "gemini-pro"
+        const model = genAI.getGenerativeModel({ model: modelName });
+        const data = imageParts.length > 0 ? [prompt, ...imageParts] : prompt;
+        const result = await model.generateContent(data);
+        const response = await result.response;
+        const text = response.text();
+        return NextResponse.json(text);
+    } catch (error) {
+        return NextResponse.json({ msg: error })
+    }
 }
