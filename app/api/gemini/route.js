bb50b4e7478f765739e1f35fb49e4ce0518b057e
karamsafarli <karamslee007@gmail.com>
2023-12-21 00:35:33 +0400

    testing new api

--- app/api/gemini/route.js
+++ app/api/gemini/route.js
@@ -1,5 +1,15 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
+import { GoogleGenerativeAI } from '@google/generative-ai'
 
 export const GET = async (req, res) => {
-    return NextResponse.json({ msg: 'hello' })
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+    const { prompt, imageParts } = await req.json();
+    // console.log(imageParts)
+    const modelName = imageParts.length > 0 ? "gemini-pro-vision" : "gemini-pro"
+    const model = genAI.getGenerativeModel({ model: modelName });
+    const data = imageParts.length > 0 ? [prompt, ...imageParts] : prompt;
+    const result = await model.generateContent(data);
+    const response = await result.response;
+    const text = response.text();
+    return NextResponse.json(text);
 }
